25be88b78541e63c674ae4eef315850d623899d9
markjasongalang <markjasongalang3@gmail.com>
2025-01-29 08:26:23 +0800

    feat(admin dashboard): implement partially edit admin info functionality

--- OnlineLibrarySystem_v1/Controllers/ProfileController.cs
+++ OnlineLibrarySystem_v1/Controllers/ProfileController.cs
@@ -267,9 +267,75 @@ namespace OnlineLibrarySystem_v1.Controllers
         }
 
         // POST: Profile/EditAdminInfo
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditAdminInfo(ProfileViewModel viewModel)
         {
+            if (ViewData["UserId"] == null)
+            {
+                return RedirectToAction(
+                    nameof(AccountController.Login).ToLowerInvariant(),
+                    nameof(AccountController).Replace("Controller", "").ToLowerInvariant()
+                );
+            }
 
+            int adminId = Convert.ToInt32(ViewData["UserId"]);
+            var admin = await _context.Users.FindAsync(adminId);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            // Check if old password is correct
+            var passwordVerificationResult = _passwordHasher.VerifyHashedPassword(
+                admin,
+                admin.PasswordHash,
+                viewModel.OldPassword
+            );
+
+            if (passwordVerificationResult == PasswordVerificationResult.Failed)
+            {
+                ModelState.AddModelError("OldPassword", "Incorrect password");
+            }
+
+            // Check if new password length is at least 5 characters
+            if (viewModel.NewPassword.Length < 5)
+            {
+                ModelState.AddModelError("NewPassword", "Must be at least 5 characters");
+            }
+
+            // Check if passwords match
+            if (viewModel.NewPassword != viewModel.ConfirmNewPassword)
+            {
+                ModelState.AddModelError("ConfirmNewPassword", "Must match with new password");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                viewModel.User = admin;
+                // TODO: Retrieve data here
+
+                return View(nameof(Admin), viewModel);
+            }
+
+            // Update new admin info
+            try
+            {
+                string hashedPassword = _passwordHasher.HashPassword(admin, viewModel.NewPassword);
+                admin.PasswordHash = hashedPassword;
+
+                _context.Users.Update(admin);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                ModelState.AddModelError("ConfirmNewPassword", "Unable to save changes. Please try again.");
+
+                viewModel.User = admin;
+                // TODO: Retrieve data here
+
+                return View(nameof(Admin), viewModel);
+            }
 
             return RedirectToAction(nameof(Admin));
         }
